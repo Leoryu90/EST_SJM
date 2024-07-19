c2c5a84e55c38bb40ed551b31fb70dc5b83b3208
Leoryu90 <thetein@gmail.com>
2024-07-19 23:43:35 +0900

    :tada: 20240719 Spring JPA MySQL

---
+++ src/main/java/com/estspringtest/est_sjm/EstSjmApplication.java
@@ -0,0 +1,13 @@
+package com.estspringtest.est_sjm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstSjmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstSjmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/estspringtest/est_sjm/EstSjmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estspringtest.est_sjm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstSjmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
